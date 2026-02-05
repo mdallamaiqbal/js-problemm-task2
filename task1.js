f191fe21d118ef02e67d07d472361bd7ace67709
mdallamaiqbal <bd999plus13@gmail.com>
2026-02-05 21:10:38 +0600

    task1 add

---
+++ task1.js
@@ -0,0 +1,15 @@
+/* 
+Find the lowest number in the array below.
+*/
+const heights = [167, 190, 120, 165, 137];
+function lowestNumber(numbers){
+   let min=heights[0];
+   for(const number of numbers){
+    if(number<min){
+        min=number
+    }
+   }
+   return min;
+}
+const minNumber=lowestNumber(heights);
+console.log(minNumber);
